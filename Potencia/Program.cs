208da23825f974617d0e152b4a1d999f3f6e9109
Roberto Andrade Salazar <andrade.roberto@uabc.edu.mx>
2022-04-07 22:56:52 -0700

    Agregar archivos de proyecto.

---
+++ Potencia/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Potencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, exponente, i;
+            double resultado=1;
+
+            Console.WriteLine("Dame un numero: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dame el exponente: ");
+            exponente = Convert.ToInt32(Console.ReadLine());    
+            Console.ReadKey();
+
+            Console.WriteLine("El resultado es: " + resultado);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Primos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, divisor, numDivisores = 0;
+            for (numero=2; numero <=100; numero++)
+
+            {
+                for(divisor = 1; divisor <= numero; divisor++)
+                {
+                    if (numero % divisor == 0) ;
+                    { 
+
+                    }
+                }    
+            }
+        }
+    }
+}
